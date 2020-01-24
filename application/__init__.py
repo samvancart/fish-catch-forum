ea5fc7013a19ed6eee73c6e990658fa99a17d10d
samvancart <sam02cart@gmail.com>
2020-01-24 18:47:57 +0200

    add new to db and list

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///images.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
 from application import views
+
+from application.images import models
+from application.images import views
+  
+db.create_all()
