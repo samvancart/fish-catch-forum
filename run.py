fb1d46655965d229d6b8f224bb81f9190fdd8068
samvancart <sam02cart@gmail.com>
2020-02-14 11:15:30 +0200

    fix

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from application import app
+import logging
+import sys
+
+# heroku debug
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run(debug=True)
